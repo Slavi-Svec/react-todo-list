5f303ff0ccaa1765a2abf05eb14d0e1541157c30
izaak svec <slavi.svec@gmail.com>
2020-03-28 17:18:54 +1000

    update Js file add Todos

---
+++ src/components/Todos.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Todos extends Component {
+    render()   {
+        return this.props.todos.map(function);
+    
+    }
+}
+
+export default Todos;
